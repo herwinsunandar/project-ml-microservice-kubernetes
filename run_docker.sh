6b5e1dba7e597006abce29a1f042355bb8fc1ccf
Herwin Sunandar <herwin.sunandar@gmail.com>
2020-05-17 13:42:03 +0700

    first commit

---
+++ run_docker.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+
+# Step 1:
+# Build image
+docker build --tag=flasksklearn .
+
+# Step 2: 
+# List docker images
+docker image ls
+
+# Step 3: 
+# Run flask app
+#docker run -p 8080:6001 flasksklearn
+docker run -p 8000:80 flasksklearn
